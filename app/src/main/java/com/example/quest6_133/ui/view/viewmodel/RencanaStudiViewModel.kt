5dd394dfcef04f62f63edab917f01df7751f508f
DaffaPutra7 <putradika3604@gmail.com>
2024-11-22 23:02:53 +0700

    Mengisi file RencanaStudiViewModel.kt dengan membuat _krsState sebagai private val, membuat krsStateUi sebagai val umum. Membuat fungsi setMatakuliah dan fungsi setKelas

--- app/src/main/java/com/example/quest6_133/ui/view/viewmodel/RencanaStudiViewModel.kt
+++ app/src/main/java/com/example/quest6_133/ui/view/viewmodel/RencanaStudiViewModel.kt
@@ -1,1 +1,22 @@
 package com.example.quest6_133.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.quest6_133.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudiViewModel: ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String) {
+        _krsState.update { stateKelas -> stateKelas.copy(kelasPilihan) }
+    }
+
+}
