f4d7eab18f0a9880b9f811203d8ba9fab721abe2
DaffaPutra7 <putradika3604@gmail.com>
2024-11-22 23:25:41 +0700

    Mengisi file PengelolaHalaman.kt dengan membuat enum class Halaman dengan mendefinisikan 4 halaman utama yaitu, Splash, Mahasiswa, RencanaStudi dan Tampil. Membuat fungsi MahasiswaApp yang didalamnya terdapat 4 parameter, modifer, mahasiswaViewModel, krsViewModel dan navController. Membuat val statusUI dan val krsSrateUi

--- app/src/main/java/com/example/quest6_133/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/quest6_133/navigation/PengelolaHalaman.kt
@@ -1,1 +1,30 @@
 package com.example.quest6_133.navigation
+
+import RencanaStudiViewModel
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.quest6_133.ui.view.viewmodel.MahasiswaViewModel
+
+enum class Halaman {
+    Splash,
+    Mahasiswa,
+    RencanaStudi,
+    Tampil
+}
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudiViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){
+    val statusUI = mahasiswaViewModel.statusUI.collectAsState().value
+    val krsStateUi = krsViewModel.krsStateUi.collectAsState().value
+
+
+}
