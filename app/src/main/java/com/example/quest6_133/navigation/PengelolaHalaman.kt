316b3333bf2f275a46585b129626e2d69a17e7d3
DaffaPutra7 <putradika3604@gmail.com>
2024-11-22 23:29:18 +0700

    Membuat NavHost sebagai fungsi utama untuk mengatur rute navigasi, dengan 3 parameter, navController, startDestination, modifier. Membuat rute navigasi ke Halaman.Splash dan ke Halaman.Mahasiswa

--- app/src/main/java/com/example/quest6_133/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/quest6_133/navigation/PengelolaHalaman.kt
@@ -1,12 +1,17 @@
 package com.example.quest6_133.navigation
 
 import RencanaStudiViewModel
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.quest6_133.ui.view.screen.MahasiswaFormView
+import com.example.quest6_133.ui.view.screen.SplashView
 import com.example.quest6_133.ui.view.viewmodel.MahasiswaViewModel
 
 enum class Halaman {
@@ -26,5 +31,29 @@ fun MahasiswaApp(
     val statusUI = mahasiswaViewModel.statusUI.collectAsState().value
     val krsStateUi = krsViewModel.krsStateUi.collectAsState().value
 
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = Modifier.padding()
+    ){
+        composable(route = Halaman.Splash.name) {
+            SplashView(onMulaiButton = {
+                navController.navigate(
+                    Halaman.Mahasiswa.name
+                )
+            })
+        }
+        composable(route = Halaman.Mahasiswa.name) {
+            MahasiswaFormView(
+                onSubmitButtonClicked = {
+                    mahasiswaViewModel.setDataSiswa(it)
+                    navController.navigate(Halaman.RencanaStudi.name)
+                },
+                onBackButtonClicked = {
+                    navController.popBackStack()
+                }
+            )
+        }
 
+    }
 }
